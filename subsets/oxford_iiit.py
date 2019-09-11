2e9de725eba2cd470e9b5fe14b58c1a7d85b2f93
Dooyoung Go <dooyounggo@gmail.com>
2019-09-11 12:32:13 +0900

    Update oxford_iiit.py

--- subsets/oxford_iiit.py
+++ subsets/oxford_iiit.py
@@ -74,8 +74,11 @@ def read_subset(subset_dir, shuffle=False, sample_size=None):
             image_dirs.append(full_filename)
 
     set_size = len(image_dirs)
-    assert len(image_dirs) == len(label_dirs),\
-        'Number of examples mismatch: {} images vs. {} labels'.format(len(image_dirs), len(label_dirs))
+    if len(label_dirs) == 0:
+        label_dirs = None
+    else:
+        assert len(image_dirs) == len(label_dirs), \
+            'Number of examples mismatch: {} images vs. {} labels'.format(len(image_dirs), len(label_dirs))
 
     if sample_size is not None and sample_size < set_size:
         if shuffle:
@@ -93,62 +96,3 @@ def read_subset(subset_dir, shuffle=False, sample_size=None):
             label_dirs = list(np.array(label_dirs)[idx])
 
     return image_dirs, label_dirs, class_names
-
-
-def read_subset_old(subset_dir, one_hot=True, sample_size=None, shuffle=True, image_size=(512, 512)):
-    filenames = os.listdir(subset_dir)
-    labels = []
-    class_names = []
-    for filename in filenames:
-        filename = filename.split('_')[:-1]
-        filename = '_'.join(filename)
-        labels.append(filename)
-        if filename not in class_names:
-            class_names.append(filename)
-    class_names.sort()
-    class_names = tuple(class_names)
-
-    num_classes = len(class_names)
-    set_size = len(filenames)
-    if sample_size is not None and sample_size < set_size:
-        if shuffle:
-            idx = np.random.choice(np.arange(set_size), size=sample_size, replace=False).astype(int)
-        else:
-            idx = np.arange(sample_size).astype(int)
-        set_size = sample_size
-    else:
-        idx = np.arange(set_size).astype(int)
-        if shuffle:
-            np.random.shuffle(idx)
-
-    X_set = np.empty((set_size, image_size[0], image_size[1], 3), dtype=np.float32)
-    Y_set = np.empty(set_size, dtype=np.uint8)
-    for i in range(set_size):
-        if i % 200 == 0:
-            print('Reading subset data: {:6d}/{}...'.format(i, set_size))
-
-        img_dir = os.path.join(subset_dir, filenames[idx[i]])
-        img = imread(img_dir)
-        if img.shape[-1] == 4:
-            img = img[:, :, 0:3]
-        if len(img.shape) == 2:
-            img = np.reshape(img, (img.shape[0], img.shape[1], 1))
-            img = np.tile(img, (1, 1, 3))
-        img = sf.resize_fit_expand(img, (image_size[0], image_size[1]), mode='constant', anti_aliasing=True)
-        X_set[i] = img
-        Y_set[i] = class_names.index(labels[idx[i]])
-
-    if one_hot:
-        Y_set_oh = np.zeros((set_size, num_classes), dtype=np.uint8)
-        Y_set_oh[np.arange(set_size), Y_set] = 1
-        Y_set = Y_set_oh
-
-    print('\nDone')
-
-    print('Data stats:')
-    print(X_set.shape)
-    print(X_set.min(), X_set.max())
-    print('{} classes:'.format(num_classes))
-    print(class_names)
-
-    return X_set, Y_set, class_names
