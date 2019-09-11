3d48f29db2df69f219a026b289aca5ae1c343cce
Dooyoung Go <dooyounggo@gmail.com>
2019-09-11 17:26:36 +0900

    Update features.py

--- features.py
+++ features.py
@@ -11,23 +11,24 @@ from parameters import Parameters
 
 Param = Parameters()
 model_to_load = Param.d['model_to_load']
-# node_names = ('block1/res0/conv2/bn', 'block2/res0/conv2/bn', 'block3/res0/conv2/bn', 'block4/res0/conv2/bn')
-node_names = ('block1/res0/conv2/bn', 'block1/att0/att_mask', 'block2/res0/conv2/bn',
-              'block2/att0/att_mask', 'block3/res0/conv2/bn', 'block3/att0/att_mask')
+feature_names = ('block_1', 'block_2', 'block_3', 'block_4')
 num_rows = 5
 num_cols = 5
 sample_size = 4
 
 # Load test set
-X_test, y_test, class_names = subset.read_subset(Param.test_dir, sample_size=sample_size, shuffle=True)
-test_set = DataSet(X_test, y_test)
+image_dirs, label_dirs, class_names = subset.read_subset(Param.test_dir, shuffle=True, sample_size=sample_size)
+Param.d['num_gpus'] = 1  # Utilizes only one GPU for convenience
+Param.d['shuffle'] = False
+test_set = DataSet(image_dirs, label_dirs, class_names, **Param.d)
 
-# Sanity check
 image_mean = np.load(os.path.join(Param.save_dir, 'img_mean.npy')).astype(np.float32)    # load mean image
 Param.d['image_mean'] = image_mean
+Param.d['monte_carlo'] = False
+Param.d['channel_first'] = False
 
 # Initialize
-model = ConvNet(Param.d['input_size'], len(class_names), y_test, **Param.d)
+model = ConvNet(Param.d['input_size'], len(class_names), loss_weights=None, **Param.d)
 saver = tf.train.Saver()
 
 if model_to_load is None:
@@ -38,31 +39,18 @@ else:
     fp = open(os.path.join(Param.save_dir, 'checkpoints.txt'), 'r')
     ckpt_list = fp.readlines()
     fp.close()
-    ckpt_to_load = ckpt_list[model_to_load][:-1]
+    ckpt_to_load = os.path.join(Param.save_dir, ckpt_list[model_to_load].rstrip())
 
 saver.restore(model.session, ckpt_to_load)    # restore learned weights
-images, y_pred = model.session.run([model.X_all, model.pred], feed_dict={model.X_in: X_test,
-                                                                         model.Y_in: y_test,
-                                                                         model.is_train: False,
-                                                                         model.monte_carlo: False,
-                                                                         model.augmentation: False})
-images = images + image_mean
+test_x, test_y_true, test_y_pred, _ = model.predict(test_set, verbose=True, **Param.d)
 
-num_gpus = Param.d.get('num_gpus', 1)
-for i in range(sample_size):
-    utils.imshow(images[i], y_test[i], y_pred[i], class_names=class_names, figure_title='Input image {}'.format(i))
+utils.plot_class_results(test_x, test_y_true, test_y_pred, fault=None, shuffle=False, class_names=class_names)
 
-for node in node_names:
-    feat_tensors = [model.dicts[i][node] for i in range(num_gpus)]
-    feat = model.session.run(feat_tensors, feed_dict={model.X_in: X_test,
-                                                      model.Y_in: y_test,
-                                                      model.is_train: False,
-                                                      model.monte_carlo: False,
-                                                      model.augmentation: False})
-    features = np.concatenate(feat, axis=0)
+tensors = [model.d[name] for name in feature_names]
+features = model.features(test_set, tensors, **Param.d)
 
-    for i in range(sample_size):
-        utils.show_features(features[i], group_features=True, num_rows=num_rows, num_cols=num_cols,
-                            figure_title='Image {} '.format(i) + node)
-
+for i, feat in enumerate(features):
+    for n in range(sample_size):
+        utils.show_features(feat[n], group_features=True, num_rows=num_rows, num_cols=num_cols,
+                            figure_title='Image {} '.format(n) + feature_names[i - 1])
 plt.show()
