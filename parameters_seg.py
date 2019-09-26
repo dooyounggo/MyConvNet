c78250ac34c53822d4a9118eb9d7a8a0248659f7
Dooyoung Go <dooyounggo@gmail.com>
2019-09-26 15:57:34 +0900

    Update parameters_seg.py

--- parameters_seg.py
+++ parameters_seg.py
@@ -85,6 +85,8 @@ class Parameters(object):
     d['augment_pred'] = False   # Online augmentation for validation or test data
 
     d['zero_pad_ratio'] = 0.0   # Zero padding ratio = (zero_padded_image_size - nn_input_size)/nn_input_size
+    
+    d['rand_blur_stddev'] = 2.5  # Maximum sigma for Gaussian blur
 
     d['rand_affine'] = True  # Bool
     d['rand_scale'] = (1.0, 1.0)  # Minimum and maximum scaling factors (x/y)
