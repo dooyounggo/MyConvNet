c79cd097f3f4339ab2675bd99424f48d74714408
Dooyoung Go <dooyounggo@gmail.com>
2019-10-08 14:48:32 +0900

    Update test_seg.py

--- test_seg.py
+++ test_seg.py
@@ -9,10 +9,15 @@ from parameters_seg import *
 
 Param = Parameters()
 model_to_load = Param.d['model_to_load']
+idx_start = 0
+idx_end = 10000
 
 # Load test set
 image_dirs, label_dirs, class_names = subset.read_subset(Param.test_dir, shuffle=False,
                                                          sample_size=Param.test_sample_size)
+num_data = len(image_dirs)
+image_dirs = image_dirs[idx_start:min(num_data, idx_end)]
+label_dirs = label_dirs[idx_start:min(num_data, idx_end)]
 Param.d['shuffle'] = False
 test_set = DataSet(image_dirs, label_dirs, class_names, **Param.d)
 
@@ -41,7 +46,8 @@ test_score = evaluator.score(test_y_true, test_y_pred)
 
 print(evaluator.name + ': {:.4f}'.format(test_score))
 
-utils.plot_seg_results(test_x, test_y_true, test_y_pred, save_dir=os.path.join(Param.save_dir, 'results_test'))
+utils.plot_seg_results(test_x, test_y_true, test_y_pred, save_dir=os.path.join(Param.save_dir, 'results_test'),
+                       start_idx=idx_start)
 plt.show()
 
 model.session.close()
