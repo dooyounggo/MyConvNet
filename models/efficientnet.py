1d2bd74998b10b4d618a6b5aebdb8540f856101a
Dooyoung Go <dooyounggo@gmail.com>
2019-10-23 16:37:27 +0900

    Update efficientnet.py

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -8,7 +8,7 @@ class EfficientNet(ConvNet):
         self.channels = [32, 16, 24, 40, 80, 112, 192, 320, 1280]
         self.kernels = [3, 3, 3, 5, 3, 5, 5, 3]
         self.strides = [2, 1, 2, 2, 2, 1, 2, 1]
-        self.res_units = [1, 2, 2, 3, 3, 4, 1]
+        self.conv_units = [1, 2, 2, 3, 3, 4, 1]
         self.multipliers = [1, 6, 6, 6, 6, 6, 6]
 
         self.se_reduction = 4
@@ -24,7 +24,7 @@ class EfficientNet(ConvNet):
         channels = self.channels
         kernels = self.kernels
         strides = self.strides
-        res_units = self.res_units
+        res_units = self.conv_units
         multipliers = self.multipliers
 
         len_c = len(channels)
@@ -54,8 +54,8 @@ class EfficientNet(ConvNet):
                     s = 1
                 else:
                     s = strides[i]
-                x = self._res_unit(x, kernels[i], s, channels[i], multipliers[i - 1], d,
-                                   drop_rate=dr, name='block_{}/res_{}'.format(i, j))
+                x = self._mb_conv_unit(x, kernels[i], s, channels[i], multipliers[i - 1], d,
+                                       drop_rate=dr, name='block_{}/mbconv_{}'.format(i, j))
             d['block_{}'.format(self._curr_block)] = x
             
         self._curr_block += 1
@@ -92,7 +92,7 @@ class EfficientNet(ConvNet):
 
         return d
 
-    def _res_unit(self, x, kernel, stride, out_channels, multiplier, d, drop_rate=0.0, name='res_unit'):
+    def _mb_conv_unit(self, x, kernel, stride, out_channels, multiplier, d, drop_rate=0.0, name='mbconv'):
         in_channels = x.get_shape()[1] if self.channel_first else x.get_shape()[-1]
         if not isinstance(stride, (list, tuple)):
             stride = [stride, stride]
@@ -100,12 +100,8 @@ class EfficientNet(ConvNet):
             stride = [stride[0], stride[0]]
 
         with tf.variable_scope(name):
-            if stride[0] == 1 and stride[1] == 1:
-                if in_channels != out_channels:
-                    with tf.variable_scope('conv_skip'):
-                        skip = self.conv_layer(x, 1, 1, out_channels, padding='SAME')
-                else:
-                    skip = x
+            if stride[0] == 1 and stride[1] == 1 and in_channels == out_channels:
+                skip = x
             else:
                 skip = None
             d[name + '/branch'] = skip
@@ -119,8 +115,6 @@ class EfficientNet(ConvNet):
                     d[name + '/conv_0' + '/bn'] = x
                     x = self.swish(x, name='swish')
                     d[name + '/conv_0' + '/swish'] = x
-                else:
-                    x = tf.identity(x)
 
             with tf.variable_scope('conv_1'):
                 x = self.conv_layer(x, kernel, stride, in_channels*multiplier,
@@ -192,67 +186,67 @@ class EfficientNetB0(EfficientNet):  # 224
     def _init_params(self):
         super()._init_params()
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB1(EfficientNet):  # 240
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.0)
-        self.res_units = self._calc_depths(self.res_units, 1.1)
+        self.conv_units = self._calc_depths(self.conv_units, 1.1)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB2(EfficientNet):  # 260
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.1)
-        self.res_units = self._calc_depths(self.res_units, 1.2)
+        self.conv_units = self._calc_depths(self.conv_units, 1.2)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB3(EfficientNet):  # 300
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.2)
-        self.res_units = self._calc_depths(self.res_units, 1.4)
+        self.conv_units = self._calc_depths(self.conv_units, 1.4)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB4(EfficientNet):  # 380
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.4)
-        self.res_units = self._calc_depths(self.res_units, 1.8)
+        self.conv_units = self._calc_depths(self.conv_units, 1.8)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB5(EfficientNet):  # 456
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.6)
-        self.res_units = self._calc_depths(self.res_units, 2.2)
+        self.conv_units = self._calc_depths(self.conv_units, 2.2)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB6(EfficientNet):  # 528
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 1.8)
-        self.res_units = self._calc_depths(self.res_units, 2.6)
+        self.conv_units = self._calc_depths(self.conv_units, 2.6)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
 
 
 class EfficientNetB7(EfficientNet):  # 600
     def _init_params(self):
         super()._init_params()
         self.channels = self._calc_widths(self.channels, 2.0)
-        self.res_units = self._calc_depths(self.res_units, 3.1)
+        self.conv_units = self._calc_depths(self.conv_units, 3.1)
         print('Widths:', self.channels)
-        print('Depths:', self.res_units)
+        print('Depths:', self.conv_units)
