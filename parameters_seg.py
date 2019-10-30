8e6b00a7f6effb80e6c6a22c3955c1b395531376
Dooyoung Go <dooyounggo@gmail.com>
2019-10-31 08:03:17 +0900

    Update parameters_seg.py

--- parameters_seg.py
+++ parameters_seg.py
@@ -68,7 +68,7 @@ class Parameters(object):
     d['gradient_threshold'] = 5.0  # Gradient thresholding using global norm. None for no thresholding
     d['loss_weighting'] = None  # None, 'balanced', [class0_weight, class1_weight, ...]. Loss = -w_c*log(y_c)
     d['focal_loss_factor'] = 0.0  # Focal_loss = -log(y_c)*(1 - y_c)^focal_loss_factor
-    d['sigmoid_focal_loss_factor'] = 0.0  # SFL = -log(y_c)*(1 - sigmoid(SFL_factor*(1 - 2*y_c))
+    d['sigmoid_focal_loss_factor'] = 0.0  # SFL = -log(y_c)*(1 - sigmoid(SFL_factor*(y_c - 0.5))
     d['loss_scaling_factor'] = 1  # Loss scaling factor for half precision training
 
     d['learning_rate_decay_method'] = 'cosine'  # 'step', 'exponential', 'cosine' (default)
