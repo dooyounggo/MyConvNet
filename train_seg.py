cf46ae215c2069599f160a0b20bce79f77478342
Dooyoung Go <dooyounggo@gmail.com>
2019-11-25 12:04:51 +0900

    Update train_seg.py

--- train_seg.py
+++ train_seg.py
@@ -1,12 +1,5 @@
-import os
-import shutil
-import numpy as np
-import tensorflow as tf
-from dataset import DataSet
-import utils
-import matplotlib.pyplot as plt
-from optimizers import MomentumOptimizer as Optimizer
 from parameters_seg import *
+import shutil
 
 
 Param = Parameters()
