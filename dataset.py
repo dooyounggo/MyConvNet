d1abb6886eb808c3cd51c8808c1727f1201cd7d5
Dooyoung Go <dooyounggo@gmail.com>
2019-12-12 16:58:36 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -260,7 +260,7 @@ class DataSet(object):
         w = self.examples_per_class.sum()/self.num_classes/self.examples_per_class
         w[np.where(self.examples_per_class == 0)] = 1.0
 
-        # FIXME: Re-balancing based on effective gradient magnitudes
+        # FIXME: Re-balancing based on effective gradient magnitudes (experimental)
         w = np.sqrt(w)
         alpha = self.num_classes/(1/w).sum()
         w = w/alpha
