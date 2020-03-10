b0ee87bb832f010b4c9c0c8967c30fd0d931fdf4
Dooyoung Go <dooyounggo@gmail.com>
2020-03-10 17:14:06 +0900

    Update parameters_adv.py

--- classification/parameters_adv.py
+++ classification/parameters_adv.py
@@ -4,7 +4,7 @@ Setup various hyperparameters
 
 import os
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import matplotlib.pyplot as plt
 from dataset import DataSet
 from subsets.cub_200_2011 import read_subset
