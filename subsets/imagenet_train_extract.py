c13731f1dce6fa46046fa831b32d1e632c435ca5
Dooyoung Go <dooyounggo@gmail.com>
2020-03-16 14:32:06 +0900

    Update imagenet_train_extract.py

--- subsets/imagenet_train_extract.py
+++ subsets/imagenet_train_extract.py
@@ -8,7 +8,10 @@ import tarfile
 
 def train_extract(train_dir, remove_tar=True):
     files = os.listdir(train_dir)
-    for i, f in enumerate(files):
+    i = 0
+    for f in files:
+        if f.split('.')[-1].lower() != 'tar':
+            continue
         if i % 100 == 0:
             print('Extracting...\t{}/1,000'.format(i))
 
@@ -17,14 +20,15 @@ def train_extract(train_dir, remove_tar=True):
         tar = tarfile.open(fname, 'r:')
         tar.extractall(path=os.path.join(train_dir, fname.split('.')[0]))
         tar.close()
-
         if remove_tar:
             os.remove(fname)
+
+        i += 1
 
     print('Extraction complete.')
 
 
 if __name__ == '__main__':
-    train_dir = './ILSVRC2012_img_train'
+    train_dir = 'D:/ILSVRC2012_img_train'
     remove_tar = True
     train_extract(train_dir, remove_tar)
