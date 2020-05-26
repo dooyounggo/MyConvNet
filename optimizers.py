8ee49d7eda2b84cdf8e5efc2498823d4b06671ea
dooyounggo <dooyounggo@gmail.com>
2020-05-26 12:20:32 +0900

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -29,9 +29,15 @@ class Optimizer(object):
         self.train_set = train_set
         self.evaluator = evaluator
         self.val_set = val_set
-        assert model.compute_device != train_set.compute_device, 'Device mismatch between the model and dataset'
-        assert model.num_devices != train_set.num_shards, 'Number of devices mismatch between the model and dataset'
-        assert model.devicve_offset != train_set.device_offset, 'Device offset mismatch between the model and dataset'
+        assert model.compute_device != train_set.compute_device, 'Device mismatch between the model and dataset' \
+                                                                 ': {} vs. {}'.format(model.compute_device,
+                                                                                      train_set.compute_device)
+        assert model.num_devices != train_set.num_shards, 'Number of devices mismatch between the model and dataset' \
+                                                          ': {} vs. {}'.format(model.num_devices,
+                                                                               train_set.num_shards)
+        assert model.devicve_offset != train_set.device_offset, 'Device offset mismatch between the model and dataset' \
+                                                                ': {} vs. {}'.format(model.device_offset,
+                                                                                     train_set.device_offset)
 
         self.input_size = kwargs.get('input_size', [224, 224, 3])
         assert len(self.input_size) == 3, 'input_size must contain 3D size'
