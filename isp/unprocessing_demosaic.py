5d065ca8fb3ceba5ca55bef5c337a62ece45cdcb
dooyounggo <dooyounggo@gmail.com>
2020-06-05 16:33:00 +0900

    Add unprocessing_demosaic.py

---
+++ isp/unprocessing_demosaic.py
@@ -0,0 +1,188 @@
+"""
+Build ISP networks with unprocessing using TensorFlow low-level APIs.
+Performs both denoising and demosaicing.
+"""
+
+from abc import abstractmethod
+import tensorflow.compat.v1 as tf
+from isp.unprocessing import Unprocessing
+from isp import process, unprocess
+
+
+class UnprocessingDemosaic(Unprocessing):
+    def _init_model(self, **kwargs):
+        output_shapes = ([None, None, None, self.input_size[-1]],
+                         None)
+        with tf.device(self.param_device):
+            with tf.variable_scope('calc/'):
+                with tf.variable_scope('sobel'):
+                    sobel_x = tf.constant([[1, 0, -1], [2, 0, -2], [1, 0, -1]], dtype=tf.float32)[..., None, None]
+                    sobel_y = tf.constant([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=tf.float32)[..., None, None]
+                    sobel_x = tf.tile(sobel_x, [1, 1, 3, 1])
+                    sobel_y = tf.tile(sobel_y, [1, 1, 3, 1])
+                    self.sobel_filter = tf.concat([sobel_x, sobel_y], axis=-1)
+        with tf.variable_scope(tf.get_variable_scope()):
+            for i in range(self.device_offset, self.num_devices + self.device_offset):
+                self._curr_device = i
+                self._curr_block = None
+                self._curr_dependent_op = 0  # For ops with dependencies between GPUs such as BN
+                with tf.device('/{}:'.format(self.compute_device) + str(i)):
+                    with tf.name_scope('{}'.format(self.compute_device + str(i))):
+                        handle = tf.placeholder(tf.string, shape=[], name='handle')  # Handle for the feedable iterator
+                        self.handles.append(handle)
+                        iterator = tf.data.Iterator.from_string_handle(handle, (tf.float32, tf.float32),
+                                                                       output_shapes=output_shapes)
+                        self.X, _ = iterator.get_next()
+                        self.X_in.append(self.X)
+
+                        if self._padded_size[0] > self.input_size[0] or self._padded_size[1] > self.input_size[1]:
+                            self.X = self.zero_pad(self.X, pad_value=self.pad_value)
+                        self.X = tf.cond(self.augmentation,
+                                         lambda: self.augment_images(self.X, **kwargs),
+                                         lambda: self.center_crop(self.X),
+                                         name='augmentation')
+
+                        image, bayer_img, noisy_img, variance, metadata = tf.map_fn(self.unprocess_images, self.X,
+                                                                                    dtype=(tf.float32, tf.float32,
+                                                                                           tf.float32, tf.float32,
+                                                                                           [tf.float32, tf.float32,
+                                                                                            tf.float32, tf.float32]),
+                                                                                    parallel_iterations=32,
+                                                                                    back_prop=False)
+
+                        self.Y = self.process(image, metadata[2], metadata[3], metadata[0])
+                        self.Y.set_shape([None] + list(self.input_size))
+                        self.Y_mosaic = process.process(bayer_img, metadata[2], metadata[3], metadata[0])
+                        self.Y_mosaic.set_shape([None] + list(self.input_size))
+                        noisy = process.process(noisy_img, metadata[2], metadata[3], metadata[0])
+                        noisy.set_shape([None] + list(self.input_size))
+                        self.Xs.append(noisy)
+                        self.Ys.append(self.Y)
+
+                        self.X = tf.concat([noisy_img, variance], axis=-1)
+                        self.X = tf.math.subtract(self.X, self.image_mean, name='zero_center')
+                        if self.channel_first:
+                            self.X = tf.transpose(self.X, perm=[0, 3, 1, 2])
+                        if self.dtype is not tf.float32:
+                            with tf.name_scope('{}/cast/'.format(self.compute_device + str(i))):
+                                self.X = tf.cast(self.X, dtype=self.dtype)
+
+                        with tf.name_scope('nn'):
+                            self.d = self._build_model()
+                        if self.dtype is not tf.float32:
+                            with tf.name_scope('{}/cast/'.format(self.compute_device + str(i))):
+                                self.d['pred'] = tf.cast(self.d['pred'], dtype=tf.float32)
+                        if self.channel_first:
+                            self.d['pred'] = tf.transpose(self.d['pred'], perm=[0, 2, 3, 1])
+                        tf.get_variable_scope().reuse_variables()
+
+                        self.dicts.append(self.d)
+                        self.pred = self.process(self.d['pred'], metadata[2], metadata[3], metadata[0])
+                        self.pred.set_shape([None] + list(self.input_size))
+                        if 'denoised' in self.d:
+                            if self.dtype is not tf.float32:
+                                with tf.name_scope('{}/cast/'.format(self.compute_device + str(i))):
+                                    self.d['denoised'] = tf.cast(self.d['denoised'], dtype=tf.float32)
+                            if self.channel_first:
+                                self.d['denoised'] = tf.transpose(self.d['denoised'], perm=[0, 2, 3, 1])
+                            self.denoised = process.process(self.d['denoised'], metadata[2], metadata[3], metadata[0])
+                            self.denoised.set_shape([None] + list(self.input_size))
+                        else:
+                            self.denoised = None
+                        self.preds.append(self.pred)
+                        self.losses.append(self._build_loss(**kwargs))
+
+        with tf.device(self.param_device):
+            with tf.variable_scope('calc/'):
+                self.X_all = tf.concat(self.Xs, axis=0, name='x')
+                self.Y_all = tf.concat(self.Ys, axis=0, name='y_true')
+                self.pred = tf.concat(self.preds, axis=0, name='y_pred')
+                self.loss = tf.reduce_mean(self.losses, name='mean_loss')
+
+                self.input_images = tf.concat(self.X_in, axis=0, name='x_in')
+                self.input_labels = self.input_images
+                self.debug_images_0 = self.Y_all
+                self.debug_images_1 = self.pred
+
+    def _build_loss(self, **kwargs):
+        edge_loss_factor = kwargs.get('edge_loss_factor', 0.0)
+        denoising_loss_factor = kwargs.get('denoising_loss_factor', 1.0)
+        with tf.variable_scope('loss'):
+            mask = tf.ones([1, self.input_size[0] - 4, self.input_size[1] - 4, 1])
+            mask = tf.pad(mask, [[0, 0], [2, 2], [2, 2], [0, 0]], constant_values=0.0)  # Mask distorted boundaries
+            loss = tf.losses.absolute_difference(mask*self.Y, mask*self.pred)
+            if edge_loss_factor > 0.0:
+                edge_y = tf.nn.depthwise_conv2d(self.Y, self.sobel_filter, strides=[1, 1, 1, 1], padding='SAME')
+                edge_pred = tf.nn.depthwise_conv2d(self.pred, self.sobel_filter, strides=[1, 1, 1, 1], padding='SAME')
+                loss += edge_loss_factor*tf.losses.mean_squared_error(mask*edge_y, mask*edge_pred)
+            if denoising_loss_factor > 0.0 and self.denoised is not None:
+                loss += denoising_loss_factor*tf.losses.absolute_difference(mask*self.Y_mosaic, mask*self.denoised)
+        return loss
+
+    @abstractmethod
+    def _build_model(self):
+        """
+        Build model.
+        This should be implemented.
+        :return dict containing tensors. Must include 'pred' tensor.
+        """
+        pass
+
+    def process(self, images, red_gains, blue_gains, cam2rgbs):
+        images.shape.assert_is_compatible_with([None, None, None, 3])
+        with tf.name_scope(None, 'process'):
+            # White balance.
+            images = self.apply_gains(images, red_gains, blue_gains)
+            images = tf.clip_by_value(images, 0.0, 1.0)
+            # Color correction.
+            images = process.apply_ccms(images, cam2rgbs)
+            # Gamma compression.
+            images = tf.clip_by_value(images, 0.0, 1.0)
+            images = process.gamma_compression(images)
+        return images
+
+    def apply_gains(self, images, red_gains, blue_gains):
+        green_gains = tf.ones_like(red_gains)
+        gains = tf.stack([red_gains, green_gains, blue_gains], axis=-1)
+        gains = gains[:, tf.newaxis, tf.newaxis, :]
+        return images*gains
+
+    def unprocess(self, image):
+        with tf.name_scope(None, 'unprocess'):
+            image.shape.assert_is_compatible_with([None, None, 3])
+
+            # Randomly creates image metadata.
+            rgb2cam = unprocess.random_ccm()
+            cam2rgb = tf.matrix_inverse(rgb2cam)
+            rgb_gain, red_gain, blue_gain = unprocess.random_gains()
+
+            # Approximately inverts global tone mapping.
+            image = unprocess.inverse_smoothstep(image)
+            # Inverts gamma compression.
+            image = unprocess.gamma_expansion(image)
+            # Inverts color correction.
+            image = unprocess.apply_ccm(image, rgb2cam)
+            # Approximately inverts white balance and brightening.
+            image = unprocess.safe_invert_gains(image, rgb_gain, red_gain, blue_gain)
+            # Clips saturated pixels.
+            image = tf.clip_by_value(image, 0.0, 1.0)
+            # Applies a Bayer mosaic.
+            bayer_image = unprocess.mosaic(image)
+
+            metadata = {
+                'cam2rgb': cam2rgb,
+                'rgb_gain': rgb_gain,
+                'red_gain': red_gain,
+                'blue_gain': blue_gain,
+            }
+            return image, bayer_image, metadata
+
+    def unprocess_images(self, image):
+        image, bayer_image, metadata = self.unprocess(image)
+        shot_noise, read_noise = unprocess.random_noise_levels()
+        noisy_img = unprocess.add_noise(bayer_image, shot_noise, read_noise)
+        variance = shot_noise*noisy_img + read_noise
+
+        metadata = [v for v in metadata.values()]
+        return image, bayer_image, noisy_img, variance, metadata
+
