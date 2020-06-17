c1b396e772a4f7550ebf1e115b77fe321c3b126e
dooyounggo <dooyounggo@gmail.com>
2020-06-17 15:03:30 +0900

    Update unprocessing.py

--- isp/unprocessing.py
+++ isp/unprocessing.py
@@ -301,5 +301,6 @@ class Unprocessing(ConvNet):
         pred = pred.reshape([num_examples*self.input_size[0], self.input_size[1], -1])
         image = np.concatenate([noisy, gt, pred], axis=1)
 
-        cv2.imwrite(os.path.join(save_dir, 'epoch_{:03d}.jpg'.format(epoch)), to_int(image),
+        image = cv2.cvtColor(to_int(image), cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join(save_dir, 'epoch_{:03d}.jpg'.format(epoch)), image,
                     [cv2.IMWRITE_JPEG_QUALITY, 100])
