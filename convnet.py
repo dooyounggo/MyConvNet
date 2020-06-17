33bae72433651f7db671c27d8a53737f35f62597
dooyounggo <dooyounggo@gmail.com>
2020-06-17 10:12:31 +0900

    Update convnet.py

--- convnet.py
+++ convnet.py
@@ -597,7 +597,7 @@ class ConvNet(object):
 
         return features
 
-    def save_results(self, dataset, save_dir, max_examples=None, **kwargs):
+    def save_results(self, dataset, save_dir, max_examples=None, **kwargs):  # Save intermediate results every epoch
         pass
 
     def cond(self, pred, true_fn, false_fn, name=None):
