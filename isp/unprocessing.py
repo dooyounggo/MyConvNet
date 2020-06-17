7c73dfb928529a68ce0d38af3bf3cd3e55318647
dooyounggo <dooyounggo@gmail.com>
2020-06-17 12:13:36 +0900

    Update unprocessing.py

--- isp/unprocessing.py
+++ isp/unprocessing.py
@@ -17,21 +17,7 @@ class Unprocessing(ConvNet):
     def _init_model(self, **kwargs):
         output_shapes = ([None, None, None, self.input_size[-1]],
                          None)
-        with tf.device(self.param_device):
-            with tf.variable_scope('calc/'):
-                with tf.variable_scope('edge'):
-                    filters = list()
-                    filters.append(tf.constant([[-1, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, -1, 0], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, -1], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, 0], [-1, 1, 0], [0, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, 0], [0, 1, -1], [0, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [-1, 0, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [0, -1, 0]], dtype=tf.float32))
-                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [0, 0, -1]], dtype=tf.float32))
-                    for i in range(len(filters)):
-                        filters[i] = tf.tile(filters[i][..., tf.newaxis, tf.newaxis], [1, 1, 3, 1])
-                    self.edge_filters = tf.concat(filters, axis=-1)
+        self._make_filters()
         self.Y_edges = []
         self.pred_edges = []
         with tf.variable_scope(tf.get_variable_scope()):
@@ -55,11 +41,12 @@ class Unprocessing(ConvNet):
                                          lambda: self.center_crop(self.X),
                                          name='augmentation')
 
-                        bayer_img, noisy_img, variance, metadata = tf.map_fn(self.unprocess_images, self.X,
-                                                                             dtype=(tf.float32, tf.float32, tf.float32,
-                                                                                    [tf.float32, tf.float32,
-                                                                                     tf.float32, tf.float32]),
-                                                                             parallel_iterations=32, back_prop=False)
+                        _, bayer_img, noisy_img, variance, metadata = tf.map_fn(self.unprocess_images, self.X,
+                                                                                dtype=(tf.float32, tf.float32,
+                                                                                       tf.float32, tf.float32,
+                                                                                       [tf.float32, tf.float32,
+                                                                                        tf.float32, tf.float32]),
+                                                                                parallel_iterations=32, back_prop=False)
 
                         self.Y = process.process(bayer_img, metadata[2], metadata[3], metadata[0])
                         self.Y.set_shape([None] + list(self.input_size))
@@ -91,6 +78,81 @@ class Unprocessing(ConvNet):
                         self.preds.append(self.pred)
                         self.losses.append(self._build_loss(**kwargs))
 
+        self._make_debug_images()
+
+    def _build_loss(self, **kwargs):
+        with tf.variable_scope('loss'):
+            loss = self._loss_fn(**kwargs)
+        return loss
+
+    def _loss_fn(self, **kwargs):
+        edge_loss_l1_factor = kwargs.get('edge_loss_l1_factor', 0.0)
+        edge_loss_l2_factor = kwargs.get('edge_loss_l2_factor', 0.0)
+        mask = tf.ones([1, self.input_size[0] - 4, self.input_size[1] - 4, 1])
+        mask = tf.pad(mask, [[0, 0], [2, 2], [2, 2], [0, 0]], constant_values=0.0)  # Mask distorted boundaries
+        loss = tf.losses.absolute_difference(mask*self.Y, mask*self.pred)
+        if edge_loss_l1_factor > 0.0 or edge_loss_l2_factor > 0.0:
+            with tf.variable_scope('edge_loss'):
+                tr = kwargs.get('edge_loss_true_ratio', 0.0)
+                edge_y = tf.nn.depthwise_conv2d(self.Y, self.edge_filters,
+                                                strides=[1, 1, 1, 1], padding='SAME')
+                edge_pred = tf.nn.depthwise_conv2d(self.pred, self.edge_filters,
+                                                   strides=[1, 1, 1, 1], padding='SAME')
+                edge_y *= mask
+                edge_pred *= mask
+
+                num_filters = self.edge_filters.get_shape().as_list()[-1]
+                y_sum = 0
+                pred_sum = 0
+                for i in range(num_filters):
+                    y_sum += edge_y[..., i*3:(i + 1)*3]**2
+                    pred_sum += edge_pred[..., i*3:(i + 1)*3]**2
+                self.Y_edges.append(tf.math.sqrt(y_sum))
+                self.pred_edges.append(tf.math.sqrt(pred_sum))
+
+                true_edge = tf.math.sqrt(tf.math.reduce_sum(edge_y**2, axis=-1, keepdims=True))
+                edge_l1 = tf.math.reduce_mean(((1.0 - tr) + tr*true_edge)*tf.math.abs(edge_y - edge_pred))
+                edge_l2 = tf.math.reduce_mean(((1.0 - tr) + tr*true_edge)*tf.math.pow(edge_y - edge_pred, 2))
+                edge_l2 = tf.math.sqrt(edge_l2 + 1e-5)
+                loss += edge_l1*edge_loss_l1_factor + edge_l2*edge_loss_l2_factor
+
+        l1_factor = kwargs.get('l1_reg', 0e-8)
+        l2_factor = kwargs.get('l2_reg', 1e-4)
+        variables = tf.get_collection('weight_variables')
+        if kwargs.get('bias_norm_decay', False):
+            variables += tf.get_collection('bias_variables') + tf.get_collection('norm_variables')
+        with tf.variable_scope('l1_loss'):
+            if l1_factor > 0.0:
+                l1_factor = tf.constant(l1_factor, dtype=tf.float32, name='L1_factor')
+                l1_reg_loss = l1_factor*tf.accumulate_n([tf.reduce_sum(tf.math.abs(var)) for var in variables])
+            else:
+                l1_reg_loss = tf.constant(0.0, dtype=tf.float32, name='0')
+        with tf.variable_scope('l2_loss'):
+            if l2_factor > 0.0:
+                l2_factor = tf.constant(l2_factor, dtype=tf.float32, name='L2_factor')
+                l2_reg_loss = l2_factor*tf.math.accumulate_n([tf.nn.l2_loss(var) for var in variables])
+            else:
+                l2_reg_loss = tf.constant(0.0, dtype=tf.float32, name='0')
+        return loss + l1_reg_loss + l2_reg_loss
+
+    def _make_filters(self):
+        with tf.device(self.param_device):
+            with tf.variable_scope('calc/'):
+                with tf.variable_scope('edge'):
+                    filters = list()
+                    filters.append(tf.constant([[-1, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, -1, 0], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, -1], [0, 1, 0], [0, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, 0], [-1, 1, 0], [0, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, 0], [0, 1, -1], [0, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [-1, 0, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [0, -1, 0]], dtype=tf.float32))
+                    filters.append(tf.constant([[0, 0, 0], [0, 1, 0], [0, 0, -1]], dtype=tf.float32))
+                    for i in range(len(filters)):
+                        filters[i] = tf.tile(filters[i][..., tf.newaxis, tf.newaxis], [1, 1, 3, 1])
+                    self.edge_filters = tf.concat(filters, axis=-1)
+                    
+    def _make_debug_images(self):
         with tf.device(self.param_device):
             with tf.variable_scope('calc/'):
                 self.X_all = tf.concat(self.Xs, axis=0, name='x')
@@ -109,57 +171,6 @@ class Unprocessing(ConvNet):
                 self.debug_images.append(edge_pred)
                 self.debug_images.append(tf.math.abs(self.Y_all - self.pred, name='image_diff'))
                 self.debug_images.append(tf.math.abs(edge_true - edge_pred, name='edge_diff'))
-
-    def _build_loss(self, **kwargs):
-        edge_loss_l1_factor = kwargs.get('edge_loss_l1_factor', 0.0)
-        edge_loss_l2_factor = kwargs.get('edge_loss_l2_factor', 0.0)
-        with tf.variable_scope('loss'):
-            mask = tf.ones([1, self.input_size[0] - 4, self.input_size[1] - 4, 1])
-            mask = tf.pad(mask, [[0, 0], [2, 2], [2, 2], [0, 0]], constant_values=0.0)  # Mask distorted boundaries
-            loss = tf.losses.absolute_difference(mask*self.Y, mask*self.pred)
-            if edge_loss_l1_factor > 0.0 or edge_loss_l2_factor > 0.0:
-                with tf.variable_scope('edge_loss'):
-                    tr = kwargs.get('edge_loss_true_ratio', 0.0)
-                    edge_y = tf.nn.depthwise_conv2d(self.Y, self.edge_filters,
-                                                    strides=[1, 1, 1, 1], padding='SAME')
-                    edge_pred = tf.nn.depthwise_conv2d(self.pred, self.edge_filters,
-                                                       strides=[1, 1, 1, 1], padding='SAME')
-                    edge_y *= mask
-                    edge_pred *= mask
-
-                    num_filters = self.edge_filters.get_shape().as_list()[-1]
-                    y_sum = 0
-                    pred_sum = 0
-                    for i in range(num_filters):
-                        y_sum += edge_y[..., i*3:(i + 1)*3]**2
-                        pred_sum += edge_pred[..., i*3:(i + 1)*3]**2
-                    self.Y_edges.append(tf.math.sqrt(y_sum))
-                    self.pred_edges.append(tf.math.sqrt(pred_sum))
-
-                    true_edge = tf.math.sqrt(tf.math.reduce_sum(edge_y**2, axis=-1, keepdims=True))
-                    edge_l1 = tf.math.reduce_mean(((1.0 - tr) + tr*true_edge)*tf.math.abs(edge_y - edge_pred))
-                    edge_l2 = tf.math.reduce_mean(((1.0 - tr) + tr*true_edge)*tf.math.pow(edge_y - edge_pred, 2))
-                    edge_l2 = tf.math.sqrt(edge_l2 + 1e-5)
-                    loss += edge_l1*edge_loss_l1_factor + edge_l2*edge_loss_l2_factor
-
-            l1_factor = kwargs.get('l1_reg', 0e-8)
-            l2_factor = kwargs.get('l2_reg', 1e-4)
-            variables = tf.get_collection('weight_variables')
-            if kwargs.get('bias_norm_decay', False):
-                variables += tf.get_collection('bias_variables') + tf.get_collection('norm_variables')
-            with tf.variable_scope('l1_loss'):
-                if l1_factor > 0.0:
-                    l1_factor = tf.constant(l1_factor, dtype=tf.float32, name='L1_factor')
-                    l1_reg_loss = l1_factor*tf.accumulate_n([tf.reduce_sum(tf.math.abs(var)) for var in variables])
-                else:
-                    l1_reg_loss = tf.constant(0.0, dtype=tf.float32, name='0')
-            with tf.variable_scope('l2_loss'):
-                if l2_factor > 0.0:
-                    l2_factor = tf.constant(l2_factor, dtype=tf.float32, name='L2_factor')
-                    l2_reg_loss = l2_factor*tf.math.accumulate_n([tf.nn.l2_loss(var) for var in variables])
-                else:
-                    l2_reg_loss = tf.constant(0.0, dtype=tf.float32, name='0')
-            return loss + l1_reg_loss + l2_reg_loss
 
     @abstractmethod
     def _build_model(self):
@@ -188,14 +199,44 @@ class Unprocessing(ConvNet):
 
         return x
 
+    def unprocess(self, image):
+        with tf.name_scope(None, 'unprocess'):
+            image.shape.assert_is_compatible_with([None, None, 3])
+
+            # Randomly creates image metadata.
+            rgb2cam = unprocess.random_ccm()
+            cam2rgb = tf.matrix_inverse(rgb2cam)
+            rgb_gain, red_gain, blue_gain = unprocess.random_gains()
+
+            # Approximately inverts global tone mapping.
+            image = unprocess.inverse_smoothstep(image)
+            # Inverts gamma compression.
+            image = unprocess.gamma_expansion(image)
+            # Inverts color correction.
+            image = unprocess.apply_ccm(image, rgb2cam)
+            # Approximately inverts white balance and brightening.
+            image = unprocess.safe_invert_gains(image, rgb_gain, red_gain, blue_gain)
+            # Clips saturated pixels.
+            image = tf.clip_by_value(image, 0.0, 1.0)
+            # Applies a Bayer mosaic.
+            bayer_image = unprocess.mosaic(image)
+
+            metadata = {
+                'cam2rgb': cam2rgb,
+                'rgb_gain': rgb_gain,
+                'red_gain': red_gain,
+                'blue_gain': blue_gain,
+            }
+            return image, bayer_image, metadata
+
     def unprocess_images(self, image):
-        image, metadata = unprocess.unprocess(image)
+        image, bayer_image, metadata = self.unprocess(image)
         shot_noise, read_noise = unprocess.random_noise_levels()
-        noisy_img = unprocess.add_noise(image, shot_noise, read_noise)
+        noisy_img = unprocess.add_noise(bayer_image, shot_noise, read_noise)
         variance = shot_noise*noisy_img + read_noise
 
         metadata = [v for v in metadata.values()]
-        return image, noisy_img, variance, metadata
+        return image, bayer_image, noisy_img, variance, metadata
 
     def save_results(self, dataset, save_dir, epoch, max_examples=None, **kwargs):
         os.makedirs(save_dir, exist_ok=True)
